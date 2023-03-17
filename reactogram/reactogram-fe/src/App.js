53a03651f1c460674eae6a7d9b45b8cf1a1a15a0
prabhjyot009 <prabh156dps@gmail.com>
2023-03-17 17:33:50 +0530

    commit

--- reactogram/reactogram-fe/src/App.js
+++ reactogram/reactogram-fe/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <>
-      <div>Bye Bye</div>
-      <Login />
-    </>
+    <div>
+      {/*<Login />*/}
+      <Signup />
+    </div>
   );
 }
 
