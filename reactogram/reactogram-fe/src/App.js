785174d126f44607b11c668ebf86a593e182784b
prabhjyot009 <prabh156dps@gmail.com>
2023-03-17 15:33:05 +0530

    commit

---
+++ reactogram/reactogram-fe/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <>
+      <div>Bye Bye</div>
+      <Login />
+    </>
+  );
+}
+
+export default App;
