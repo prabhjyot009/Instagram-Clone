31813cab4cc9993168823e5fa15a8970f2b95ba5
prabhjyot009 <prabh156dps@gmail.com>
2023-03-18 05:30:19 +0530

    commit

--- reactogram/reactogram-fe/src/pages/Profile.js
+++ reactogram/reactogram-fe/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Profile.css'
+
 const Profile = () => {
     return (
         <div className='container shadow mt-3 p-4'>
@@ -14,21 +15,21 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className='col-md-6 d-flex flex-column justify-content-between mt-3'>
-                    <div className='d-flex justify-content-equal'>
-                        <div className="count-section pe-5 text-center fw-bold">
+                    <div className='d-flex justify-content-equal mx-auto'>
+                        <div className="count-section px-4 pe-md-5 text-center fw-bold">
                             <h4>10</h4>
                             <p>Posts</p>
                         </div>
-                        <div className='count-section px-5 text-center fw-bold'>
+                        <div className='count-section px-4 px-md-5 text-center fw-bold'>
                             <h4>20</h4>
                             <p>Followers</p>
                         </div>
-                        <div className='ps-5 text-center fw-bold'>
+                        <div className='ps-md-5 ps-4 text-center fw-bold'>
                             <h4>20</h4>
                             <p>Folllowing</p>
                         </div>
                     </div>
-                    <div className='mx-auto'>
+                    <div className='mx-auto mt-md-0 mt-3'>
                         <button className="custom-btn custom-btn-white me-md-3">
                             <span className='fs-6'>Edit Profile</span>
                         </button>
@@ -38,6 +39,46 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
+            <div className='row my-3'>
+                <div className='col-12'>
+                    <hr />
+                </div>
+            </div>
+            <div className='row mb-4'>
+                <div className='col-md-4 col-sm-12'>
+                    <div class="card" >
+                        <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="Trees" />
+                    </div>
+                </div>
+                <div className='col-md-4 col-sm-12'>
+                    <div class="card" >
+                        <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="Trees" />
+                    </div>
+                </div>
+                <div className='col-md-4 col-sm-12'>
+                    <div class="card" >
+                        <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="Trees" />
+                    </div>
+                </div>
+            </div>
+            <div className='row mb-4'>
+                <div className='col-md-4 col-sm-12'>
+                    <div class="card" >
+                        <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="Trees" />
+                    </div>
+                </div>
+                <div className='col-md-4 col-sm-12'>
+                    <div class="card" >
+                        <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="Trees" />
+                    </div>
+                </div>
+                <div className='col-md-4 col-sm-12'>
+                    <div class="card" >
+                        <img src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="Trees" />
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
